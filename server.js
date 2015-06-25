bde7c260831481effbcea006a91ce1a6d6218c9d
Shulamyt <shulamyt@gmail.com>
2015-06-25 13:49:00 +0300

    add server
    add server

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var server = app.listen(3000, function () {});
+
+app.get('/oreli', function (req, res) {
+  res.send('<div>Hello Oreli!</div>');
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
