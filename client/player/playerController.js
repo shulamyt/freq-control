e834cf3ecbd31acb9f8ae133e08036b175e21acd
Isaacgl68 <isaacgl68@gmail.com>
2015-07-01 23:10:08 +0300

    .

--- client/player/playerController.js
+++ client/player/playerController.js
@@ -2,7 +2,9 @@ angular.module('FreqControl')
     .controller('playerController', ['$scope', '$rootScope', '$location', '$http', '$sce','socket',
         function($scope, $rootScope, $location, $http, $sce, socket) {
             $scope.youtubeURL = $scope.songsList[ $scope.currentSongIndex].url;
-
+            $scope.isPlay = true;
+            $scope.isMute = false;
+            $scope.playClass = "glyphicon-pause";
             $scope.clickNextSong = function() {
                 $scope.nextSong();
                 socket.emit('nextSong');
@@ -45,43 +47,61 @@ angular.module('FreqControl')
 
 
             $scope.clickPlayVideo = function() {
-                $scope.playVideo();
-                socket.emit('play');
+                if ($scope.isPlay)
+                {
+                    $scope.stopVideo();
+                    socket.emit('stop');
+                    $scope.playClass = "glyphicon-play";
+                }
+                else
+                {
+                    $scope.playVideo();
+                    socket.emit('play');
+                    $scope.playClass = "glyphicon-pause";
+
+                }
+
+
             };
 
             $scope.playVideo = function(){
                 $scope.ytPlayer.playVideo();
+                $scope.isPlay = true;
             }
 
             socket.on('play', $scope.playVideo);
 
-            $scope.clickStopVideo = function() {
-                $scope.stopVideo();
-                socket.emit('stop');
-            };
             $scope.stopVideo = function(){
                 $scope.ytPlayer.stopVideo();
+                $scope.isPlay = false;
                 }
 
             socket.on('stop', $scope.stopVideo);
 
             $scope.clickMute = function() {
-                $scope.mute();
-                socket.emit('mute');
+                if($scope.isMute)
+                {
+                    $scope.unMute();
+                    socket.emit('unMute');
+                }else
+                {
+                    $scope.mute();
+                    socket.emit('mute');
+                }
+
             };
 
             $scope.mute = function() {
+                $scope.isMute = true;
                 $scope.ytPlayer.mute();
             };
 
             socket.on('mute', $scope.mute);
 
-            $scope.clickUnMute = function() {
-                $scope.unMute();
-                socket.emit('unMute');
-            };
+
 
             $scope.unMute = function() {
+                $scope.isMute = false;
                 $scope.ytPlayer.unMute();
             };
 
@@ -93,10 +113,16 @@ angular.module('FreqControl')
             };
 
             $scope.volumeUp = function() {
+                if($scope.isMute)
+                {
+                    $scope.unMute();
+                }
                 var currentVolume = $scope.ytPlayer.getVolume();
                 if(currentVolume<100){
                     $scope.ytPlayer.setVolume(currentVolume + 5);
+
                 }
+                //$scope.isMute = false;
             }
 
             socket.on('volumeUp', $scope.volumeUp);
@@ -115,6 +141,9 @@ angular.module('FreqControl')
                 var currentVolume = $scope.ytPlayer.getVolume();
                 if(currentVolume>0){
                     $scope.ytPlayer.setVolume(currentVolume - 5);
+                }else if (currentVolume <=0)
+                {
+                    $scope.isMute = true;
                 }
             }
 
