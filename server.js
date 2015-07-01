53b09086b7276ec8fd07109fd15c9cd3b5aae6ab
orelil <oreli.levi@gmail.com>
2015-07-01 14:11:33 +0300

    remoce examples

--- server.js
+++ server.js
@@ -6,22 +6,6 @@ var io = require('socket.io')(server);
 server.listen(3000, function () {});
 app.use(express.static('client'));
 
-app.get('/Shula', function (req, res) {
-  res.send('<div>Hello Shula!</div>');
-});
-
-app.get('/Meital', function (req, res) {
-  res.send('<div>Hello Meital!</div>');
-});
-
-app.get('/Oreli', function (req, res) {
-  res.send('<div>Hello Oreli!</div>');
-});
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
 io.on('connection', function(socket){
 	console.log('user connected');
 	 socket.on('newMessage', function(message){
