fe372ff9e24daebf1d5a27d8a46ace7b0ee96a31
orelil <oreli.levi@gmail.com>
2015-07-01 11:42:49 +0300

    angular changes

---
+++ client/playlist/playListController.js
@@ -0,0 +1,6 @@
+angular.module('FreqControl')
+    .controller('playListController', ['$scope', '$rootScope', '$location', '$http', '$sce',
+        function($scope, $rootScope, $location, $http, $sce) {
+
+
+        }]);
