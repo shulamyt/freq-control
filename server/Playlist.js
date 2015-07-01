678cc68b139d306f36f2f191296cf9cf36d9aa82
meitalbe <meitalbe@amdocs.com>
2015-07-01 14:17:58 +0300

    Initial server classes skeleton

---
+++ server/Playlist.js
@@ -0,0 +1,16 @@
+/**
+ * Created by MEITALBE on 7/1/2015.
+ */
+var db = require('./DB');
+
+module.exports = {
+    addPlaylist: function (data) {
+        // whatever
+    },
+    removePlaylist: function (data) {
+        // whatever
+    },
+    getPlaylist: function (data) {
+        // whatever
+    }
+};
---
+++ server/Song.js
@@ -0,0 +1,28 @@
+/**
+ * Created by MEITALBE on 7/1/2015.
+ */
+var db = require('./DB');
+
+module.exports = {
+    addSong: function (data) {
+        // whatever
+    },
+    getSong: function (data) {
+        // whatever
+    },
+    getSongs: function (data) {
+        // whatever
+    },
+    removeSong: function (data) {
+        // whatever
+    },
+    removeAllSongs: function (data) {
+        // whatever
+    },
+    dislikeSong: function (data) {
+        // whatever
+    },
+    likeSong: function (data) {
+        // whatever
+    }
+};
