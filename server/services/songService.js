95310b788a6d1759c691d36cedb6ca24b9410723
meitalbe <meitalbe@amdocs.com>
2015-07-01 16:44:39 +0300

    Return previous deleted functions

--- server/services/songService.js
+++ server/services/songService.js
@@ -1,11 +1,10 @@
 module.exports = function (app, socket) {
+
     /*Create new song*/
-
-
-
     app.post('/song', function (req, res) {
         var name = req.body.name;
         var url = req.body.url;
+
         socket.then(function(socketObj){
             socketObj.on('newMessage', function(message){
                 socketObj.broadcast.emit('new message', {
@@ -14,6 +13,37 @@ module.exports = function (app, socket) {
                 });
             });
         })
-
     });
+
+    /*Get song by ID*/
+    app.get('/song', function (req, res) {
+        var id = req.body.id;
+
+        // TODO DB
+    });
+
+    /*Delete song by ID*/
+    app.delete('/song', function (req, res) {
+        var id = req.body.id;
+
+        // TODO DB
+        // TODO push
+    });
+
+    /*Mark Like a song by ID*/
+    app.post('/song/like', function (req, res) {
+        var id = req.body.id;
+
+        // TODO DB
+        // TODO push
+    });
+
+    /*Mark Dislike a song by ID*/
+    app.post('/song/dislike', function (req, res) {
+        var id = req.body.id;
+
+        // TODO DB
+        // TODO push
+    });
+
 };
