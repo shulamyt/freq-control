20839f81a4bb0b555314c1fbe4c61c012b665c56
orelil <oreli.levi@gmail.com>
2015-07-01 11:28:56 +0300

    socket.io integration

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-app.listen(3000, function () {});
+server.listen(3000, function () {});
 app.use(express.static('client'));
 
 app.get('/Shula', function (req, res) {
@@ -18,5 +20,15 @@ app.get('/Oreli', function (req, res) {
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+io.on('connection', function(socket){
+	console.log('user connected');
+	 socket.on('newMessage', function(message){
+	 	 socket.broadcast.emit('new message', {
+       username: socket.username,
+       message: message
+     });
+	 	});
 });
 
