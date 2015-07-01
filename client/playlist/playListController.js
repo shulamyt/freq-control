b1656befb4e4f2ef4995088e7255548ab4948694
Isaacgl68 <isaacgl68@gmail.com>
2015-07-01 16:18:27 +0300

    add song form

--- client/playlist/playListController.js
+++ client/playlist/playListController.js
@@ -1,10 +1,17 @@
 angular.module('FreqControl')
     .controller('playListController',
         function($scope, $modal) {
+            $scope.newSong = {};
             $scope.title = "Add Song";
-            var addSongModal = $modal({scope: $scope, template: 'playlist/addSongForm.html', show: false});
+            var addSongModal = $modal({scope: $scope, template: 'playlist/addSongForm.html', show: false, backdrop:'static'});
             $scope.onAddClick = function () {
+                $scope.newSong = {};
                 addSongModal.$promise.then(addSongModal.show);
+            }
+
+            $scope.saveNewSong = function () {
+                console.log("song is: " + angular.toJson($scope.newSong));
+                addSongModal.hide();
             }
 
         });
