0a2a0546e2b31f598049d42534b88722c68b59a5
amitrintzler <amitrintzler@gmail.com>
2015-07-01 16:55:06 +0300

    control buttons
    adding the buttons

--- client/player/playerController.js
+++ client/player/playerController.js
@@ -1,6 +1,34 @@
 angular.module('FreqControl')
     .controller('playerController', ['$scope', '$rootScope', '$location', '$http', '$sce',
         function($scope, $rootScope, $location, $http, $sce) {
-            $scope.youtubeURL = 'https://www.youtube.com/watch?v=yd8jh9QYfEs';
+            $scope.youtubeURL = 'https://www.youtube.com/watch?v=LO2uPU53qds';
+
+            $scope.playVideo = function(){
+                $scope.ytPlayer.playVideo();
+                }
+
+            $scope.stopVideo = function(){
+                $scope.ytPlayer.stopVideo();
+                }
+
+            $scope.muteVolume = function() {
+                $scope.ytPlayer.mute();
+            }
+            $scope.unMuteVolume = function() {
+                $scope.ytPlayer.unMute();
+            }
+
+            $scope.volumeUp = function() {
+                currentVolume = $scope.ytPlayer.getVolume();
+                if(currentVolume<100){
+                    $scope.ytPlayer.setVolume(currentVolume + 5);
+                }
+            }
+            $scope.volumeDown = function() {
+                currentVolume = $scope.ytPlayer.getVolume();
+                if(currentVolume>0){
+                    $scope.ytPlayer.setVolume(currentVolume - 5);
+                }
+            }
     }]
 );
